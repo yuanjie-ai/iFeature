06a6b64a1db5c0a353bbc853310c30781704ae25
尐 <313303303@qq.com>
2018-07-23 10:23:51 +0800

    Create __init__.py

---
+++ ifeature/featadd/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+__title__ = '__init__.py'
+__author__ = 'JieYuan'
+__mtime__ = '2018/7/23'
+"""
+
+from ._NumericalFeature import NumericalFeature
+from ._CategoryFeature import CategoryFeature
+from ._TimeFeature import TimeFeature
