56abaa43242249d4b8b7e8f20b6549a31e1a8376
尐 <313303303@qq.com>
2018-07-26 17:47:16 +0800

    Update _TimeFeature.py

--- ifeature/featadd/_TimeFeature.py
+++ ifeature/featadd/_TimeFeature.py
@@ -11,28 +11,28 @@ class TimeFeature(object):
         pass
 
     @staticmethod
-    def get_feats_time(data, group_col=None, time_col='CREATETIME', feat_cols=None):
+    def get_feats_time(df, group_col=None, time_col='CREATETIME', feat_cols=None):
         """
-        :param data:
+        :param df:
         :param group_col:
         :param time_col:
         :param feat_cols:
         :return:
         """
-        print('time continuous ...')
-        data[time_col + '_year'] = data[time_col].apply(lambda x: x.year)
-        data[time_col + '_month'] = data[time_col].apply(lambda x: x.month)
-        data[time_col + '_day'] = data[time_col].apply(lambda x: x.day)
-        data[time_col + '_weekday'] = data[time_col].apply(lambda x: x.weekday())
-        data[time_col + '_diff'] = data.groupby(group_col)[time_col].diff().apply(lambda x: x.days).fillna(0)
-        data[time_col + '_time_interval'] = data[time_col].transform(lambda x: x.max() - x).apply(lambda x: x.days)
+        print('Time Explodes Into Year/Month/Day ...')
+        df[time_col + '_year'] = df[time_col].apply(lambda x: x.year)
+        df[time_col + '_month'] = df[time_col].apply(lambda x: x.month)
+        df[time_col + '_day'] = df[time_col].apply(lambda x: x.day)
+        df[time_col + '_weekday'] = df[time_col].apply(lambda x: x.weekday())
+        df[time_col + '_diff'] = df.groupby(group_col)[time_col].diff().apply(lambda x: x.days).fillna(0)
+        df[time_col + '_time_interval'] = df[time_col].transform(lambda x: x.max() - x).apply(lambda x: x.days)
 
         if feat_cols:
             print("Feats Diff ...")
-            _data = data.groupby(group_col)[feat_cols].diff().fillna(0) \
+            df = df.groupby(group_col)[feat_cols].diff().fillna(0) \
                 .rename(columns={i + '_diff': i for i in feat_cols})  # 提前按时间升序排列
 
             print("Feats Average Encoding ...")
-            _data = data.groupby(time_col)[feat_cols].transform('mean') \
+            df = df.groupby(time_col)[feat_cols].transform('mean') \
                 .rename(columns={i + '_diff': i for i in feat_cols})  # median
-        return data
+        return df
