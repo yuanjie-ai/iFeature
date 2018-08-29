065cbb383c0c9e9a28de81a3638c516798e78bef
Jie-Yuan <313303303@qq.com>
2018-08-29 16:07:08 +0800

    update

--- ifeature/sub/info_value.py
+++ ifeature/sub/info_value.py
@@ -16,18 +16,19 @@ class InformationValue(object):
 
         self.label = label
         self.df = df.assign(_label=1 - df[label])
-        self.cols = [col for col in df.columns if col != label]
+        self.feats = [col for col in df.columns if col != label]
 
         self.y1 = self.df[label].values.sum()
         self.y0 = self.df['_label'].values.sum()
 
-    def iv(self, n_jobs=16):
+    def iv(self, order=True, n_jobs=16):
         with ThreadPoolExecutor(max_workers=n_jobs) as pool:
-            ivs = pool.map(self._iv_func, tqdm(self.cols, 'Calculating ...'), chunksize=1)
-        return pd.DataFrame(sorted(zip(ivs, self.cols), reverse=True), columns=['iv', 'feats'])
+            ivs = pool.map(self.__iv, tqdm(self.feats, 'Calculating ...'), chunksize=1)
+        z = zip(ivs, self.feats)
+        return pd.DataFrame(sorted(z, reverse=True) if order else list(z), columns=['iv', 'feats'])
 
-    def _iv_func(self, col):
-        gr = self.df.groupby(col)
+    def __iv(self, feat):
+        gr = self.df.groupby(feat)
         gr1, gr0 = gr[self.label].sum().values + 1e-8, gr['_label'].sum().values + 1e-8
         good, bad = gr1 / self.y1, gr0 / self.y0
         woe = np.log(good / bad)
